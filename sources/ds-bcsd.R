553952a8300ea20ce61ff289483b36a1b6e4b92f
program-- <justin@justinsingh.me>
2023-07-06 10:20:13 -0700

    ds: add BCSD

---
+++ sources/ds-bcsd.R
@@ -0,0 +1,19 @@
+.pull_bcsd <- function(...) {
+    arrow::as_arrow_table(climateR::read_dap_file(
+        URL = "https://cida.usgs.gov/thredds/dodsC/bcsd_obs",
+        id  = "bcsd"
+    ))
+}
+
+.tidy_bcsd <- function(.tbl, ...) {
+    dplyr::as_tibble(.tbl) |>
+        climateR::dap_meta() |>
+        dplyr::mutate(tiled = "", type = "opendap") |>
+        arrow::as_arrow_table()
+}
+
+ds_bcsd <- climateR.catalogs::data_source$new(
+    id   = "bcsd",
+    pull = .pull_bcsd,
+    tidy = .tidy_bcsd
+)
