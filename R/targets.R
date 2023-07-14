08cee92f03bf991d2284e7afd83409cf18a76cdd
mikejohnson51 <jmj00@ucsb.edu>
2023-07-14 14:47:26 -0600

    fix: missed spacing

--- R/targets.R
+++ R/targets.R
@@ -1019,44 +1019,41 @@ get_fcpg = function(base = 'https://prod-is-usgs-sb-prod-publish.s3.amazonaws.co
 
   o = read_json(base, simplifyVector = TRUE)
 
-meta = matrix(c('lc1',  'Percent upstream Temperate or sub-polar needleleaf forest', '%',
-  'lc2', 'Percent upstream Sub-polar taiga needleleaf forest', '%',
-  'lc3', 'Percent upstream Tropical or sub-tropical broadleaf evergreen forest', '%',
-  'lc4', 'Percent upstream Tropical or sub-tropical broadleaf deciduous forest', '%',
-  'lc5', 'Percent upstream Temperate or sub-polar broadleaf deciduous forest', '%',
-  'lc6', 'Percent upstream Mixed Forest', '%',
-  'lc7', 'Percent upstream Tropical or subtropical shrubland', '%',
-  'lc8', 'Percent upstream Temperate or sub-polar shrubland', '%',
-  'lc9', 'Percent upstream Tropical or sub-tropical grassland', '%',
-  'lc10', 'Percent upstream Temperate or sub-polar grassland', '%',
-  'lc11', 'Percent upstream Sub-polar or polar shrubland-lichen-moss', '%',
-  'lc12', 'Percent upstream Sub-polar or polar grassland-lichen-moss', '%',
-  'lc13', 'Percent upstream Sub-polar or polar barren-lichen-moss', '%',
-  'lc14', 'Percent upstream Wetland', '%',
-  'lc15', 'Percent upstream Cropland', '%',
-  'lc16', 'Barren Lands', '%',
-  'lc17', 'Urban or Built-up', '%',
-  'lc18', 'Water', '%',
-  'lc19', 'Snow and Ice', '%',
-  'prcp', 'Daymet Mean Annual Total Precipitation 1989 - 2018', "mm",
-  'elev', 'Mean elevation of grid cell as estimated by the NHD DEM', "cm",
-  'lat',  'latitude of the center of the pixel', "degrees",
-  'tmin', 'Daymet Mean Daily Minimum Temperature 1989 - 2018', "C",
-  'tmax', 'Daymet Mean Daily Maximum Temperature 1989 - 2018', "C",
-  'slope', 'Slope of the land surface', 'cm/cm',
-  'str100', 'Binary raster approximating the presence of a stream', " ",
-  'fac',  'An integer flow accumulation grid which contains the number of cells within CONUS draining to each cell within CONUS based on the HydroDEM', ""), ncol = 3, byrow = T) %>%
-  data.frame() %>%
-  setNames(c("variable", "description", 'units'))
+  meta = matrix(c('lc1',  'Percent upstream Temperate or sub-polar needleleaf forest', '%',
+    'lc2', 'Percent upstream Sub-polar taiga needleleaf forest', '%',
+    'lc3', 'Percent upstream Tropical or sub-tropical broadleaf evergreen forest', '%',
+    'lc4', 'Percent upstream Tropical or sub-tropical broadleaf deciduous forest', '%',
+    'lc5', 'Percent upstream Temperate or sub-polar broadleaf deciduous forest', '%',
+    'lc6', 'Percent upstream Mixed Forest', '%',
+    'lc7', 'Percent upstream Tropical or subtropical shrubland', '%',
+    'lc8', 'Percent upstream Temperate or sub-polar shrubland', '%',
+    'lc9', 'Percent upstream Tropical or sub-tropical grassland', '%',
+    'lc10', 'Percent upstream Temperate or sub-polar grassland', '%',
+    'lc11', 'Percent upstream Sub-polar or polar shrubland-lichen-moss', '%',
+    'lc12', 'Percent upstream Sub-polar or polar grassland-lichen-moss', '%',
+    'lc13', 'Percent upstream Sub-polar or polar barren-lichen-moss', '%',
+    'lc14', 'Percent upstream Wetland', '%',
+    'lc15', 'Percent upstream Cropland', '%',
+    'lc16', 'Barren Lands', '%',
+    'lc17', 'Urban or Built-up', '%',
+    'lc18', 'Water', '%',
+    'lc19', 'Snow and Ice', '%',
+    'prcp', 'Daymet Mean Annual Total Precipitation 1989 - 2018', "mm",
+    'elev', 'Mean elevation of grid cell as estimated by the NHD DEM', "cm",
+    'lat',  'latitude of the center of the pixel', "degrees",
+    'tmin', 'Daymet Mean Daily Minimum Temperature 1989 - 2018', "C",
+    'tmax', 'Daymet Mean Daily Maximum Temperature 1989 - 2018', "C",
+    'slope', 'Slope of the land surface', 'cm/cm',
+    'str100', 'Binary raster approximating the presence of a stream', " ",
+    'fac',  'An integer flow accumulation grid which contains the number of cells within CONUS draining to each cell within CONUS based on the HydroDEM', ""), ncol = 3, byrow = T) %>%
+    data.frame() %>%
+    setNames(c("variable", "description", 'units'))
 
- data.frame(id = "USGS FCPG", varname = o$parameters, URL =  unlist(o$urls)) %>%
-  mutate(variable = gsub("_fcpg.vrt", "", basename(URL))) %>%
-  vrt_meta() %>%
-  mutate(type = "vrt", tiled = "") %>%
-  left_join(meta, by = "variable") %>%
-  vrt_meta()
-
-
-
+   data.frame(id = "USGS FCPG", varname = o$parameters, URL =  unlist(o$urls)) %>%
+    mutate(variable = gsub("_fcpg.vrt", "", basename(URL))) %>%
+    vrt_meta() %>%
+    mutate(type = "vrt", tiled = "") %>%
+    left_join(meta, by = "variable") %>%
+    vrt_meta()
 }
 
