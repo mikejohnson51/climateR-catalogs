b655f308bac9ccdb77f08d0ecc572707ee0259fa
mikejohnson51 <jmj00@ucsb.edu>
2023-10-05 09:32:15 -0600

    rm log/fetch in prism URL mikejohnson51/climateR#82

--- sources/ds-prism_daily.R
+++ sources/ds-prism_daily.R
@@ -1,14 +1,16 @@
 .pull_prism_daily <- function(...) {
-    arrow::as_arrow_table(climateR::read_dap_file(
-        URL = "http://convection.meas.ncsu.edu:8080/thredds/dodsC/prism/daily/combo/2021/PRISM_combo_20211010.nc#log&show=fetch",
+    .tbl = arrow::as_arrow_table(climateR::read_dap_file(
+        URL = "http://convection.meas.ncsu.edu:8080/thredds/dodsC/prism/daily/combo/2021/PRISM_combo_20211010.nc",
         id  = "prism_daily"
     ))
+
+    arrow::as_arrow_table(.tbl)
 }
 
 # ---------------------------------------------------------------------
 
 .tidy_prism_daily <- function(.tbl, ...) {
-    dplyr::as_tibble(.tbl) |>
+   .tbl =  dplyr::as_tibble(.tbl) |>
         dplyr::mutate(duration = "1981-01-01/..", variable = varname) |>
         climateR::dap_meta() |>
         dplyr::mutate(
@@ -17,8 +19,9 @@
                   gsub(pattern = "20211010", replacement = "{YYYYMMDD}"),
             tiled = "T",
             interval = "1 day"
-        ) |>
-        arrow::as_arrow_table()
+        )
+
+    arrow::as_arrow_table(.tbl)
 }
 
 # ---------------------------------------------------------------------
@@ -28,3 +31,5 @@ ds_prism_daily <- climateR.catalogs::data_source$new(
     pull = .pull_prism_daily,
     tidy = .tidy_prism_daily
 )
+
+
